02e92aeb6d29a3f4138be32487d86a48a3730436
LorealCI <loreal1806@gmail.com>
2025-02-27 16:01:29 +0000

    added a Posts model

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1',
+                '.herokuapp.com']
 
 
 # Application definition
@@ -89,6 +90,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
